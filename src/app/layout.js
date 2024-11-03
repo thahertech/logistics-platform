f65c6f75aba02d588a11f13ebcf8348e3b455c25
Thaher Al Amir <thaher.alamir@hotmail.com>
2024-11-03 06:18:51 +0200

    added analytics for nextjs and google

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,7 @@
 import './globals.css';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import AnalyticsClient from './components/analyticsClient';
 
 export const metadata = {
   title: 'Logistix',
@@ -7,14 +10,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fi">
       <head>
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics /> {/* NextJS */}
+        <SpeedInsights /> {/* NextJS */}
+        <AnalyticsClient /> {/* Google */}
+      </body>
     </html>
   );
 }
