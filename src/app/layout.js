85aa52477db1dac3da18e132eaf7941de1bd0704
Thaher Al Amir <thaher.alamir@hotmail.com>
2024-11-03 05:36:32 +0200

    updated landing page intro and styling for components

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
