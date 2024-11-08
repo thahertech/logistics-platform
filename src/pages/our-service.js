a4005a1fb5b924b89cff80b2a860cfa1659b3288
Thaher Al Amir <thaher.alamir@hotmail.com>
2024-11-09 01:14:12 +0200

    our-services

--- src/pages/our-service.js
+++ src/pages/our-service.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '@/app/dashboardTEMP/Layout';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRocket, faLock, faChartLine, faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faRocket, faLeaf, faChartLine, faBriefcase } from '@fortawesome/free-solid-svg-icons';
 import styles from '../app/Styles/page.module.css';
 import '../app/globals.css';
 import Image from 'next/image';
@@ -14,10 +14,9 @@ const MeidanPalvelusta = () => {
         <h2 className={styles.DashboardTitle}>Meidän Palvelusta</h2>
       </div>
       <div className={styles.section}>
-        <h2 className={styles.sectionTitle}>Mikä Tämä Palvelu On?</h2>
+        <h2 className={styles.sectionTitle}>Mitä tämä palvelu tarjoaa?</h2>
         <p className={styles.sectionContent}>
-          Olemme digitaalinen alusta, joka yhdistää tuotteiden omistajat ja kuljetusyritykset, mahdollistaen nopean ja luotettavan toimitusprosessin. 
-          Palvelumme on suunniteltu tekemään toimitukset helpoiksi, olitpa yksityishenkilö tai yritys.
+          Digitaalinen alusta, joka yhdistää tavaran ja kuljettajat,<br/> mahdollistaen suoraviivaivsen ja luotettavan toimitusprosessin.
         </p>
       </div>
       <div className={styles.ServiceContainers}>
@@ -31,19 +30,20 @@ const MeidanPalvelusta = () => {
       <div className={styles.MasonryContainer}>
 
         <div className={styles.masonryItem}>
-          <h2 className={styles.sectionTitle}>Miten?</h2>
+          <h2 className={styles.sectionTitle}>Toimintatapa?</h2>
           <p className={styles.sectionContent}>
-            1. <strong>Julkaise Toimitus</strong>: Tuotteen omistajat voivat julkaista toimitustarpeensa nopeasti.
-            <br /> 2. <strong>Tarjoa Kuljetus</strong>: Kuljetusyritykset voivat selata avoimia toimituksia ja tarjota palveluitaan.
-            <br /> 3. <strong>Varmista Toimitus</strong>: Kun kuljetus on hyväksytty, toimitusprosessi alkaa sujuvasti.
+            1. <strong>Julkaise toimitus</strong>: Lähettäjä voi julkaista toimitustarpeen nopeasti.
+            <br /> 2. <strong>Etsi toimitus</strong>: Kuljetusyritykset selaa avoimia toimituksia ja tarjoaa.
+            <br /> 3. <strong>Hyväksy toimitus</strong>: Varmistuksen jälkeen automaattinen dokumentointi
           </p>
         </div>
 
         <div className={styles.masonryItem}>
-          <h2 className={styles.sectionTitle}>Kenelle?</h2>
+          <h2 className={styles.sectionTitle}>Sopivuus?</h2>
           <p className={styles.sectionContent}>
-            Palvelumme on ihanteellinen sekä yrityksille että yksityishenkilöille, jotka tarvitsevat luotettavan tavan hallita toimituksiaan.
-            Kuljetusyrityksille tarjoamme helpon tavan löytää uusia asiakkaita ja kasvattaa liiketoimintaansa.
+            Palvelumme on ihanteellinen sekä yrityksille että yksityishenkilöille, jotka tarvitsevat ekologisen tavan hallita toimituksiaan.<br/>
+            Kuljetusyrityksille helppo tapa löytää uusia asiakkaita ja optimoida tyhjiä kilometrejä.
+
           </p>
         </div>
 
@@ -51,7 +51,7 @@ const MeidanPalvelusta = () => {
           <h2 className={styles.sectionTitle}>Miksi Logistix?</h2>
           <ul className={styles.benefitsList}>
             <li><FontAwesomeIcon icon={faRocket} /> Nopeus: Julkaise ja kilpailuta toimituksesi muutamalla klikkauksella.</li>
-            <li><FontAwesomeIcon icon={faLock} /> Ekologisuus: Yksinkertaisesti säästät luontoa.</li>
+            <li><FontAwesomeIcon icon={faLeaf} /> Ekologisuus: Yksinkertaisesti säästät luontoa.</li>
             <li><FontAwesomeIcon icon={faChartLine} /> Kasvata Bisnestäsi: Kuljetusyritykset voivat laajentaa asiakaskuntaansa tehokkaasti.</li>
             <li><FontAwesomeIcon icon={faBriefcase} /> Helppokäyttöisyys: Palvelu on selkeä ja käyttäjäystävällinen kaikille.</li>
           </ul>
