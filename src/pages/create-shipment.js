2ae679042ae118d6d3a13c2ddc1fc8751246aec5
Thaher Al Amir <thaher.alamir@hotmail.com>
2024-11-09 02:08:05 +0200

    test

--- src/pages/create-shipment.js
+++ src/pages/create-shipment.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import dayjs from 'dayjs';
-import Layout from '../app/dashboardTEMP/Layout';
+import Layout from '../app/Dashboard/Layout';
 import '../app/globals.css';
 import { jwtDecode } from 'jwt-decode';
 
