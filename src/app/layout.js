73bfa174538e69af6b5388782a9ec0aa10947325
thahertech <thaher.alamir@hotmail.com>
2024-12-06 06:48:20 +0200

    small tiny changes..

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,8 @@
 import './globals.css';
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { GoogleAnalytics } from '@next/third-parties/google';
-import Head from 'next/head';
-import Script from 'next/script';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
+
 
 export const metadata = {
   title: 'Logistix',
@@ -14,23 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fi">
-      <Head>
-        {/* GTM script snippet for the <head> */}
-        {process.env.NEXT_PUBLIC_GTM_ID && (
-          <Script
-            id="google-tag-manager"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM_ID}';f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
-              `,
-            }}
-          />
-        )}
+      <GoogleTagManager gtmId="GTM-T7GRXLNQ" />
+
         <link rel="icon" href="/favicon.ico" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -40,19 +24,9 @@ export default function RootLayout({ children }) {
         <meta property="og:image" content="/assets/logistix-logos/png/Logo.png" />
         <meta property="og:url" content="https://www.logistix.fi" />
 
-      </Head>
       <body>
-        {/* NoScript version for users with JavaScript disabled */}
-        {process.env.NEXT_PUBLIC_GTM_ID && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-        )}
+      
+        
         {children}
         <Analytics />
         <SpeedInsights />
