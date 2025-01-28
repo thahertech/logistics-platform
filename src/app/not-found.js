9561dd95d6e5011a12ca7ed710eb8aceca4abc6a
thahertech <thaher.alamir@hotmail.com>
2025-01-28 17:25:32 +0200

    additions

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./Styles/NotFound.module.css";
+import styles from "./Styles/Notfound.module.css";
 import ParticleBackground from '../app/Components/bg-animation';
 
 const NotFound = () => {
