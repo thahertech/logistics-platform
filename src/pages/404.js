a436137f9971d6b3143d72b59954df712763620b
thahertech <thaher.alamir@hotmail.com>
2025-01-28 17:43:44 +0200

    additions to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,34 @@
+"use client";
 
-export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
-}
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../app/Styles/Notfound.module.css";
+import ParticleBackground from '../app/Components/bg-animation';
+
+const NotFound = () => {
+  return (
+    
+    <div className={styles.notFoundContainer}>
+                    <ParticleBackground/> 
+
+      {/* Hero Section */}
+      <div className={styles.heroImg}>
+       
+        </div>
+
+        <div className={styles.overlay}>
+          <h1 className={styles.title}>Sivua ei löytynyt</h1>
+          <p className={styles.description}>
+            Näyttää siltä, että etsimääsi sivua ei ole olemassa. <br />
+            Tarkista osoite tai palaa etusivulle.
+          </p>
+          <Link href="/" className={styles.link}>
+            <button className={styles.backButton}>Palaa etusivulle</button>
+          </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
