de5ffe35623084394a7e09e6cf010445d5777ce3
thahertech <thaher.alamir@hotmail.com>
2025-04-03 02:11:14 +0300

    forgot sitemap..

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -1,16 +1,24 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
 const Sitemap = (req, res) => {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+
+  const articlesDir = path.join(process.cwd(), "content/articles");
+  const filenames = fs.readdirSync(articlesDir);
+
+  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>https://www.logistix.fi/</loc>
       <lastmod>${new Date().toISOString()}</lastmod>
-      <changefreq>weekly</changefreq>
+      <changefreq>monthly</changefreq>
       <priority>1.0</priority>
     </url>
     <url>
       <loc>https://www.logistix.fi/yrityksille</loc>
       <lastmod>${new Date().toISOString()}</lastmod>
-      <changefreq>weekly</changefreq>
+      <changefreq>monthly</changefreq>
       <priority>0.9</priority>
     </url>
     <url>
@@ -24,14 +32,26 @@ const Sitemap = (req, res) => {
       <lastmod>${new Date().toISOString()}</lastmod>
       <changefreq>monthly</changefreq>
       <priority>0.7</priority>
-    </url>
-        <url>
-      <loc>https://www.logistix.fi/auth</loc>
+    </url>`;
+
+  filenames.forEach((filename) => {
+    const filePath = path.join(articlesDir, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const { data } = matter(fileContents);
+
+    const articleSlug = filename.replace(".md", "");
+    const articleUrl = `https://www.logistix.fi/articles/${articleSlug}`;
+
+    sitemap += `
+    <url>
+      <loc>${articleUrl}</loc>
       <lastmod>${new Date().toISOString()}</lastmod>
       <changefreq>monthly</changefreq>
-      <priority>0.5</priority>
-    </url>
-    
+      <priority>0.7</priority>
+    </url>`;
+  });
+
+  sitemap += `
   </urlset>`;
 
   res.setHeader('Content-Type', 'text/xml');
