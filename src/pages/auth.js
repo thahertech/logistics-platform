283c7c21a1e50b8918849ff000dc727abad75a89
thahertech <thaher.alamir@hotmail.com>
2025-04-12 16:47:43 +0300

    login, register system refactored and updated.

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { supabase } from '@/supabaseClient';
-import Layout from '../app/Dashboard/Layout';
-import FormFields from '../app/Components/forms/AuthForm';
-import Modal from '../app/Components/modals/LoginModal';
-import { handleSubmit, signInWithGoogle, signInWithApple, handleForgotPassword } from '@/app/Components/authUtils';
+import Layout from '../app/dashboard/Layout';
+import FormFields from '../app/components/forms/AuthForm';
+import { handleSubmit, signInWithGoogle, signInWithApple, handleForgotPassword } from '@/app/components/authentication/authUtils';
 import styles from '../app/Styles/login.module.css';
-import Image from 'next/image';
-import ParticleBackground from '@/app/Components/bg-animation';
+import AuthSectionAbstract from '@/app/components/animation/auth-section-abstract';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Auth = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -27,26 +27,26 @@ const Auth = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const router = useRouter();
+
   return (
     <Layout>
-      <div className={styles.bg}>
-            <ParticleBackground/> {/* Include the particle background */}
-            </div>
+      
       <div className={`${styles.authPageBackground} flex justify-center items-center`}>
-      <div className="bg-black bg-opacity-70 mt-6 backdrop-filter backdrop-blur-lg border border-gray-300 flex p-6 rounded-lg shadow-md w-full max-w-4xl">          <div className="hidden md:flex flex-1 items-center justify-center">
-            <Image
-              src={"/assets/3d.jpg"}
-              alt="sunset-image"
-              width="2000"
-              height="500"
-              className={styles.loginImg}
-            />
+        <div className="bg-black bg-opacity-70 mt-6 backdrop-filter backdrop-blur-lg border border-gray-300 flex p-6 rounded-lg shadow-md w-full max-w-4xl">         
+          <div className="hidden md:flex flex-1 items-center justify-center">
+            <AuthSectionAbstract />
           </div>
-          <div className="flex flex-col flex-1 p-4">
-            <h2 className="text-4xl font-bold mb-12 text-left text-white mb-8 pb-2.5 border-b border-white/90">
-              {isLogin ? 'Kirjaudu' : 'Luo Käyttäjä'}
-            </h2>
-            <form onSubmit={(e) => handleSubmit(e, isLogin, formData, setError, setIsLoading, setFormData, setShowModal, router)}>
+        <div className="flex flex-col flex-1 p-4">
+          <p className=" flex-row text-center justify-around text-gray-300 mb-12 ">
+              {isLogin ? (
+                <span>Kirjaudu <button onClick={() => setIsLogin(false)} className="text-white rounded-[12px] ml-8 ml-8 border py-2 px-4 ">Rekisteröidy </button></span>
+              ) : (
+                <span>Rekisteröidy<button onClick={() => setIsLogin(true)} className="text-white rounded-[12px] py-2 px-4 ml-8 border">Kirjaudu sisään</button></span>
+              )}
+            </p>
+            <form 
+            
+            onSubmit={(e) => handleSubmit(e, isLogin, formData, setError, setIsLoading, setFormData, setShowModal, setIsLogin, router, toast)}>
               <FormFields 
                 isLogin={isLogin} 
                 formData={formData} 
@@ -57,51 +57,42 @@ const Auth = () => {
                 handleForgotPassword={(e) => handleForgotPassword(e, formData, setError, setIsLoading, setShowModal)} 
               />
             </form>
-            {/* Google Sign-In Button
-            <div className="flex flex-col items-center mt-6">
-  <button
-    type="button"
-    onClick={() => signInWithGoogle(setIsLoading, setError, router)}
-    className=" flex justify-center items-center"
-  >
-    {isLoading ? (
-      <span>Loading...</span>
-    ) : (
-      <img
-        src="/assets/signin-assets/Web (mobile + desktop)/svg/dark/web_dark_rd_ctn.svg " // Path to your PNG image
-        alt="Sign in with Google"
-        width={200}
-        height={200}
-        className="cursor-pointer"
-      />
-    )}
-  </button>
-  <button 
-    type="button"
-    onClick={() => signInWithApple(setIsLoading, setError, router)}
-    className=" flex justify-center items-center mt-2"
-    >
-  <img 
-  src="/assets/apple-account-continue-with~dark@2x.png"
-  alt="sign in with apple"
-  width={200}
-  height={200}
-  className="cursor-pointer"
-/>
-</button>
-</div> */}
-            {/* Switch between Login and Register */}
-            <p className=" text-left text-gray-300">
-              {isLogin ? (
-                <span>Ei tiliä? <button onClick={() => setIsLogin(false)} className="text-white underline">Luo tili</button></span>
-              ) : (
-                <span>Onko jo tili? <button onClick={() => setIsLogin(true)} className="text-white underline">Kirjaudu sisään</button></span>
-              )}
-            </p>
+            {isLogin ? (
+
+            <div className="flex flex-row items-center justify-evenly mt-10">
+              <button
+                type="button"
+                onClick={() => signInWithGoogle(setIsLoading, setError, router)}
+                className=" flex justify-center items-center"
+              >
+                  <img
+                    src="../assets/signin-assets/iOS/png@4x/dark/ios_dark_rd_na@4x.png"
+                    alt="Sign in with Google"
+                    width={70}
+                    height={70}
+                    className="cursor-pointer"
+                  />
+          
+              </button>
+              <button 
+                type="button"
+                onClick={() => signInWithApple(setIsLoading, setError, router)}
+                className=" flex justify-center items-center mt-2"
+                >
+              <img 
+              src="../assets/signin-assets/Web (mobile + desktop)/Logo - SIWA - Logo-only - Black@3x.png"
+              alt="sign in with apple"
+              width={70}
+              height={70}
+              className="cursor-pointer"
+            />
+            </button>
+            </div>
+          ) : ( []
+            )}       
           </div>
         </div>
       </div>
-      {showModal && <Modal closeModal={() => setShowModal(false)} />}
     </Layout>
   );
 };
