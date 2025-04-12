24e59e2765fcac583b01141e667b3c372d4d4ce4
thahertech <thaher.alamir@hotmail.com>
2025-04-12 16:47:43 +0300

    login, register system refactored and updated.

--- src/app/Components/forms/AuthForm.js
+++ src/app/Components/forms/AuthForm.js
@@ -6,116 +6,143 @@ const FormFields = ({ isLogin, formData, setFormData, error, isLoading, setIsLoa
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
+
+
   return (
     <>
       {!isLogin && (
         <>
-          <div className="mb-1">
+        <div className="flex-col">
+          <label>Etunimi</label>
             <input
               type="text"
               name="name"
-              placeholder="Etunimi"
               value={formData.name}
               onChange={handleChange}
-              className={`text-black w-full p-2 mb-2 border rounded`}
+              className={`text-white bg-transparent w-full mt-2 p-2 mb-4 border rounded-[12px] `}
               required
             />
-          </div>
-          <div className="mb-1">
+          <label>Sukunimi</label>
             <input
               type="text"
               name="lastName"
-              placeholder="Sukunimi"
               value={formData.lastName}
               onChange={handleChange}
-              className={`text-black w-full p-2 mb-2 border rounded`}
+              className={`text-white w-full bg-transparent p-2 mt-2 mb-12 border rounded-[12px]`}
               required
             />
-          </div>
-          <div className="mb-1">
-            <input
-              type="text"
-              name="yTunnus"
-              placeholder="Y tunnus"
-              value={formData.yTunnus}
-              onChange={handleChange}
-              className={`text-black w-full p-2 mb-2 border rounded`}
-              required
-            />
-          </div>
-          <div className="mb-1">
-            <input
-              type="text"
-              name="phone"
-              placeholder="Puhelinnumero"
-              value={formData.phone}
-              onChange={handleChange}
-              className={`text-black w-full p-2 mb-2 border rounded`}
-              required
-            />
-          </div>
-          <div className="mb-1">
+            <label>
+              Käyttötarkoitus
+            </label>
             <select
                 name="role"
                 value={formData.role}
                 onChange={handleChange}
-                className="text-gray-600 w-full p-1 mb-2 border rounded h-10"
-                required
-            >
-                <option value="">Valitse rooli</option>
+                className="text-white mt-2 w-full border bg-transparent p-2 mb-4 rounded-[12px]"
+                required>
                 <option value="kuljettaja">Kuljettaja</option>
                 <option value="lähettäjä">Lähettäjä</option>
             </select>
-            </div>
+            <label>Yritys</label>
+            <input
+              type="text"
+              name="yritys_nimi"
+              value={formData.yritys_nimi}
+              onChange={handleChange}
+              className={`text-white bg-transparent w-full mt-2 p-2 mb-4 border rounded-[12px] `}
+              required
+            />
+          <label>
+            Y tunnus
+          </label>
+            <input
+              type="text"
+              name="yTunnus"
+              value={formData.yTunnus}
+              onChange={handleChange}
+              className={`text-white mt-2 bg-transparent w-full p-2 mb-4 border rounded-[12px]`}
+              required
+            />
+          <div className="mb-1">
+            <label>
+              Puhelinnumero
+            </label>
+            <input
+              type="text"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              className={`text-gray-300 mt-2 bg-transparent w-full p-2 mb-2 border rounded-[12px]`}
+              required
+            />
+          </div>
+          </div>
         </>
       )}
 
       <div className="mb-4 mt-12">
+        <label>
+          Sähköposti
+        </label>
         <input
           type="email"
           name="email"
           value={formData.email}
           onChange={handleChange}
-          placeholder="Sähköposti"
-          className={`text-black border border-gray-300 p-2 w-full rounded`}
+          placeholder=""
+          className={`text-white mt-2 bg-transparent border border-gray-300 p-2 w-full rounded-[12px]`}
           required
         />
       </div>
       <div className="mb-6">
+      <label>
+          Salasana
+        </label>
         <input
           type="password"
           name="password"
           value={formData.password}
           onChange={handleChange}
-          placeholder="Salasana"
-          className={`text-black border border-gray-300 p-2 w-full rounded`}
+          placeholder=""
+          className={`text-white mt-2 border bg-transparent border-gray-300 p-2 w-full rounded-[12px]`}
           required
         />
       </div>
-      <button
-        type="submit"
-        className={`w-full bg-[#003366] text-white py-2 rounded hover:bg-[#002244] transition ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
-        disabled={isLoading}
+{!isLogin && (
+  <div className="flex items-center mt-4 mb-6">
+    <input
+      type="checkbox"
+      id="terms"
+      name="terms"
+      checked={formData.termsAccepted}
+      onChange={(e) =>
+        setFormData({ ...formData, termsAccepted: e.target.checked })
+      }
+      className="mr-2 ml-1"
+    />
+    <label htmlFor="terms" className="text-gray-300 text-ml">
+      Hyväksyn{' '}
+      <a
+        href="/extras/ehdot"
+        className="underline text-white"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        {isLoading ? 'Loading...' : (isLogin ? 'Kirjaudu' : 'Luo Käyttäjä')}
-      </button>
-      {isLogin && (
-  <p className="mt-6 text-right text-gray-300">
-    <button
-      type="button"
-      className="text-white-200 underline"
-      onClick={handleForgotPassword}
-    >
-      Unohditko salasanan?
-    </button>
-  </p>
+        käyttöehdot
+      </a>
+    </label>
+  </div>
 )}
 
-{error && (
-  <div className="text-white-500 mt-8 text-center">
-    <p>{error}</p>
-  </div>
-      )}
+<button
+  type="submit"
+  className={`w-full bg-[#003366] text-white py-2 rounded-[12px] hover:bg-[#002244] transition ${
+  isLoading ? 'opacity-50 cursor-not-allowed' : ''
+  }`}
+  disabled={isLoading}
+>
+  {isLoading ? 'Odota...' : isLogin ? 'Kirjaudu sisään' : 'Luo käyttäjä'}
+</button>
     </>
   );
 };
