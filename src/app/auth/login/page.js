9a34f175ea7ec88eaa864454b1c07e7375e8234d
Thaher Al Amir <67804374+thahertech@users.noreply.github.com>
2025-05-04 14:07:30 +0300

    Update page.js

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -6,13 +6,10 @@ export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-transparent p-6 md:p-10">
       <div className="flex w-full max-w-2xl flex-col gap-6">
-//        <a href="/" className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <GalleryVerticalEnd className="size-4" />
           </div>
           Logistix.
-  //      </a>
-       // <LoginForm />
       </div>
     </div>
   )
