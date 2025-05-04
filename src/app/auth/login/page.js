bacb6899e10682465114015673bc08baacc342f9
Thaher Al Amir <67804374+thahertech@users.noreply.github.com>
2025-05-04 14:02:42 +0300

    Update page.js

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,6 +1,6 @@
 import { GalleryVerticalEnd } from "lucide-react"
 import 'tailwindcss/tailwind.css';
-import { LoginForm } from "@/components/login-form"
+//import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
@@ -12,7 +12,7 @@ export default function LoginPage() {
           </div>
           Logistix.
         </a>
-        <LoginForm />
+       // <LoginForm />
       </div>
     </div>
   )
