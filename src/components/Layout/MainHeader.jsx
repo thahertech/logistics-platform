c31af76f074190261d6491c8b220e97ff30d5cf0
Thaher Al Amir <67804374+thahertech@users.noreply.github.com>
2025-05-04 13:55:32 +0300

    Update MainHeader.jsx

--- src/components/Layout/MainHeader.jsx
+++ src/components/Layout/MainHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from '@/app/styles/layout.module.css';
+import styles from '@/app/Styles/Layout.module.css';
  import footerImg from '@/../public/assets/logistix-logos/svg/logo.svg';
 
 const MainHeader = () => {
