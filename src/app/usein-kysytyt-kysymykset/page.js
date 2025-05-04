2788e5123987670e366c59911f068eee193b85c1
Thaher Al Amir <67804374+thahertech@users.noreply.github.com>
2025-05-04 14:17:42 +0300

    Update page.js

--- src/app/usein-kysytyt-kysymykset/page.js
+++ src/app/usein-kysytyt-kysymykset/page.js
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import styles from '@/app/styles/Faq.module.css';
+import styles from '@/app/Styles/Faq.module.css';
 import '@/app/globals.css';
 import Layout from '@/components/Layout/Layout';
 
